819319113f6e76dcb4d28eb8450279121c411b51
github-account-0 <91717711+github-account-0@users.noreply.github.com>
2022-03-23 01:55:45 -0400

    Update storage.cpp

--- speeduino/storage.cpp
+++ speeduino/storage.cpp
@@ -139,6 +139,10 @@ static inline write_location writeTable(const void *pTable, table_type_t key, wr
                   write(rows_begin(pTable, key), location)));
 }
 
+//Simply an alias for EEPROM.update()
+void EEPROMWriteRaw(uint16_t address, uint8_t data) { EEPROM.update(address, data); }
+uint8_t EEPROMReadRaw(uint16_t address) { return EEPROM.read(address); }
+
 //  ================================= End write support ===============================
 
 /** Write a table or map to EEPROM storage.
@@ -522,6 +526,64 @@ uint32_t readPageCRC32(uint8_t pageNum)
 {
   uint32_t crc32_val;
   return EEPROM.get(compute_crc_address(pageNum), crc32_val);
+}
+
+/** Same as above, but writes the CRC32 for the calibration page rather than tune data
+@param pageNum - Calibration page number
+@param crcValue - CRC32 checksum
+*/
+void storeCalibrationCRC32(uint8_t calibrationPageNum, uint32_t calibrationCRC)
+{
+  uint16_t targetAddress;
+  switch(calibrationPageNum)
+  {
+    case O2_CALIBRATION_PAGE:
+      targetAddress = EEPROM_CALIBRATION_O2_CRC;
+      break;
+    case IAT_CALIBRATION_PAGE:
+      targetAddress = EEPROM_CALIBRATION_IAT_CRC;
+      break;
+    case CLT_CALIBRATION_PAGE:
+      targetAddress = EEPROM_CALIBRATION_CLT_CRC;
+      break;
+    default:
+      targetAddress = EEPROM_CALIBRATION_CLT_CRC; //Obviously should never happen
+      break;
+  }
+
+  EEPROM.put(targetAddress, calibrationCRC);
+}
+
+/** Retrieves and returns the 4 byte CRC32 checksum for a given calibration page from EEPROM.
+@param pageNum - Config page number
+*/
+uint32_t readCalibrationCRC32(uint8_t calibrationPageNum)
+{
+  uint32_t crc32_val;
+  uint16_t targetAddress;
+  switch(calibrationPageNum)
+  {
+    case O2_CALIBRATION_PAGE:
+      targetAddress = EEPROM_CALIBRATION_O2_CRC;
+      break;
+    case IAT_CALIBRATION_PAGE:
+      targetAddress = EEPROM_CALIBRATION_IAT_CRC;
+      break;
+    case CLT_CALIBRATION_PAGE:
+      targetAddress = EEPROM_CALIBRATION_CLT_CRC;
+      break;
+    default:
+      targetAddress = EEPROM_CALIBRATION_CLT_CRC; //Obviously should never happen
+      break;
+  }
+
+  EEPROM.get(targetAddress, crc32_val);
+  return crc32_val;
+}
+
+uint16_t getEEPROMSize()
+{
+  return EEPROM.length();
 }
 
 // Utility functions.
